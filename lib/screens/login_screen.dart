7beddc2e2326044fed31c0bf1fa98c2cc970790d
itsrko02 <ritwik.tiwari02@gmail.com>
2020-08-23 00:07:42 +0530

    added an app drawer, loading screen and a login screen with animations.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -80,7 +80,6 @@ class _LoginPageState extends State<LoginPage>
                       begin: Alignment.topLeft,
                       end: Alignment.bottomRight,
                       colors: [
-                        Colors.lightGreenAccent,
                         Colors.lightGreen,
                         Colors.green,
                         Colors.lightGreen,
